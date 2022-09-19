61322488cadf3b13734572949f5a9348d75b676b
BeomV <101873654+BeomV@users.noreply.github.com>
2022-09-19 13:23:00 +0900

    Greencar

--- js/main.js
+++ js/main.js
@@ -1,24 +1,34 @@
-$(document).ready(function(){
-  
-  // fullpage customization
-  $('#fullpage').fullpage({
-    sectionSelector: '.section',
-    navigation: true,
-    slidesNavigation: true,
-    css3: true,
-    controlArrows: false    
-  }); 
-  
-});
+const slides = document.querySelector('.slides'); //전체 슬라이드 컨테이너
+const slideImg = document.querySelectorAll('.slides li'); //모든 슬라이드들
+let currentIdx = 0; //현재 슬라이드 index
+const slideCount = slideImg.length; // 슬라이드 개수
+const prev = document.querySelector('.prev'); //이전 버튼
+const next = document.querySelector('.next'); //다음 버튼
+const slideWidth = 300; //한개의 슬라이드 넓이
+const slideMargin = 100; //슬라이드간의 margin 값
 
-window.onload = function() {
-  i=1;
-  setInterval(function(){
-    var pic = document.getElementById('target');
-    pic.setAttribute("src",("/images/bg_section040"+(i%4+1)+".jpg"));
-    i++;
-    
-  },5000);
+//전체 슬라이드 컨테이너 넓이 설정
+slides.style.width = (slideWidth + slideMargin) * slideCount + 'px';
 
+function moveSlide(num) {
+  slides.style.left = -num * 400 + 'px';
+  currentIdx = num;
 }
 
+prev.addEventListener('click', function () {
+  /*첫 번째 슬라이드로 표시 됐을때는 
+  이전 버튼 눌러도 아무런 반응 없게 하기 위해 
+  currentIdx !==0일때만 moveSlide 함수 불러옴 */
+
+  if (currentIdx !== 0) moveSlide(currentIdx - 1);
+});
+
+next.addEventListener('click', function () {
+  /* 마지막 슬라이드로 표시 됐을때는 
+  다음 버튼 눌러도 아무런 반응 없게 하기 위해
+  currentIdx !==slideCount - 1 일때만 
+  moveSlide 함수 불러옴 */
+  if (currentIdx !== slideCount - 2) {
+    moveSlide(currentIdx + 1);
+  }
+});
